3064dbf736d9f755ad550a73c7545daf598a1ff9
eschmidt42 <11818904+eschmidt42@users.noreply.github.com>
2026-05-08 19:43:27 +0200

    test: migrated transactions.py tests

---
+++ tests/accounts_etl/common/test_transactions.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+
+import polars as pl
+import pytest
+
+from fintl.accounts_etl.common.transactions import (
+    hash_transactions,
+    verify_transactions,
+)
+
+
+def test_hash_transactions():
+    data = {"col1": [1, 2, 3], "col2": ["a", "b", "c"]}
+    transactions = pl.DataFrame(data)
+    hash_columns = ["col1", "col2"]
+    hashed_transactions = hash_transactions(transactions, hash_columns)
+    assert "hash" in hashed_transactions.columns
+    assert len(hashed_transactions) == 3
+
+
+def test_verify_transactions(tmp_path: Path):
+    data = {"col1": [1, 2, 3], "col2": ["a", "b", "c"]}
+    transactions = pl.DataFrame(data)
+    transaction_columns = ["col1", "col2"]
+    verify_transactions(transaction_columns, transactions, tmp_path)
+
+    transaction_columns = ["col1", "col2", "col3"]
+    with pytest.raises(ValueError):
+        verify_transactions(transaction_columns, transactions, tmp_path)
--- tests/accounts_etl/providers/dkb/test_utils.py
+++ tests/accounts_etl/providers/dkb/test_utils.py
@@ -1,12 +1,7 @@
 from pathlib import Path
 
-import polars as pl
 import pytest
 
-from fintl.accounts_etl.common.transactions import (
-    hash_transactions,
-    verify_transactions,
-)
 from fintl.accounts_etl.io.files.detect import find_line_with_pattern, is_match
 
 
@@ -28,26 +23,6 @@ def test_find_line_with_pattern():
     pattern = r"\d+"
     with pytest.raises(ValueError):
         find_line_with_pattern(lines, pattern)
-
-
-def test_hash_transactions():
-    data = {"col1": [1, 2, 3], "col2": ["a", "b", "c"]}
-    transactions = pl.DataFrame(data)
-    hash_columns = ["col1", "col2"]
-    hashed_transactions = hash_transactions(transactions, hash_columns)
-    assert "hash" in hashed_transactions.columns
-    assert len(hashed_transactions) == 3
-
-
-def test_verify_transactions(tmp_path: Path):
-    data = {"col1": [1, 2, 3], "col2": ["a", "b", "c"]}
-    transactions = pl.DataFrame(data)
-    transaction_columns = ["col1", "col2"]
-    verify_transactions(transaction_columns, transactions, tmp_path)
-
-    transaction_columns = ["col1", "col2", "col3"]
-    with pytest.raises(ValueError):
-        verify_transactions(transaction_columns, transactions, tmp_path)
 
 
 def test_detect_encoding_fallback_when_chardet_returns_none(tmp_path: Path):
