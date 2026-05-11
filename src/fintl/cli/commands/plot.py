ba151ddc839b30618a14489b366ff73f85bc08d5
eschmidt42 <11818904+eschmidt42@users.noreply.github.com>
2026-05-11 15:00:54 +0200

    feat: refactored plot.py

--- src/fintl/cli/commands/plot.py
+++ src/fintl/cli/commands/plot.py
@@ -1,8 +1,10 @@
 import pathlib
 import tempfile
 import webbrowser
+from pathlib import Path
 from typing import Annotated, Optional
 
+import altair as alt
 import polars as pl
 import typer
 
@@ -17,17 +19,15 @@ def run(
 ):
     """Plot bank account balances."""
     config = Config()
-    balances = pl.read_parquet(config.target_dir / "all-balances.parquet")
-    balances = balances.with_columns(
-        name=pl.col("provider").str.to_lowercase()
-        + " "
-        + pl.col("service").str.to_lowercase()
-    )
 
-    chart = balances.plot.scatter(x="date", y="amount", color="name").properties(
-        width=600, height=400
-    )
+    balances = load_data(config)
 
+    chart = draw_plot(balances)
+
+    display_plot(save, chart)
+
+
+def display_plot(save: Path | None, chart: alt.Chart):
     if save is not None:
         chart.save(str(save))
         typer.echo(f"Chart saved to {save}")
@@ -37,3 +37,22 @@ def run(
             tmp = pathlib.Path(f.name)
         chart.save(str(tmp))
         webbrowser.open(tmp.resolve().as_uri())
+
+
+def draw_plot(balances: pl.DataFrame) -> alt.Chart:
+    chart = balances.plot.scatter(x="date", y="amount", color="name").properties(
+        width=600, height=400
+    )
+
+    return chart
+
+
+def load_data(config: Config) -> pl.DataFrame:
+    balances = pl.read_parquet(config.target_dir / "all-balances.parquet")
+    balances = balances.with_columns(
+        name=pl.col("provider").str.to_lowercase()
+        + " "
+        + pl.col("service").str.to_lowercase()
+    )
+
+    return balances
