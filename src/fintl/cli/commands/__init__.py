3c494d53c8c1505a1ef476e36f99f38d8358c32b
eschmidt42 <11818904+eschmidt42@users.noreply.github.com>
2026-05-11 14:54:26 +0200

    feat: run_plot via commands namespace

--- src/fintl/cli/commands/__init__.py
+++ src/fintl/cli/commands/__init__.py
@@ -1,3 +1,4 @@
 from fintl.cli.commands.etl import run as run_etl
+from fintl.cli.commands.plot import run as run_plot
 
-__all__ = ["run_etl"]
+__all__ = ["run_etl", "run_plot"]
--- src/fintl/cli/main.py
+++ src/fintl/cli/main.py
@@ -1,14 +1,13 @@
 import typer
 
-from fintl.cli.commands import run_etl
-from fintl.cli.commands.plot import run as plot
+from fintl.cli.commands import run_etl, run_plot
 from fintl.cli.commands.search import main as search_main
 from fintl.cli.commands.store import run as store
 
 app = typer.Typer()
 
 app.command(name="etl", help="Run the accounts ETL pipeline.")(run_etl)
-app.command(name="plot", help="Plot bank account balances.")(plot)
+app.command(name="plot", help="Plot bank account balances.")(run_plot)
 app.command(
     name="store",
     help="Store downloaded bank files into the correct ETL input directories.",
