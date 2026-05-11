ec29873fcc65752c2e6208e90b3c32e8d2fde9ce
eschmidt42 <11818904+eschmidt42@users.noreply.github.com>
2026-05-11 13:30:40 +0200

    chore: plot.py -> commands/

--- src/fintl/cli/main.py
+++ src/fintl/cli/main.py
@@ -1,9 +1,9 @@
 import typer
 
 from fintl.cli.commands.etl import run as etl
+from fintl.cli.commands.plot import run as plot
 from fintl.cli.commands.search import main as search_main
 from fintl.cli.commands.store import run as store
-from fintl.cli.plot import run as plot
 
 app = typer.Typer()
 
